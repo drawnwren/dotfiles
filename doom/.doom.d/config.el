6379a6dc97c6466ad42234c357156d41caa7846c
Wren <drawnwren@gmail.com>
2019-08-19 15:07:31 -0400

    Added doom config

---
+++ doom/.doom.d/config.el
@@ -0,0 +1,6 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+(setq
+ prettier-js-args '("--single-quote")
+ display-line-numbers-type 'relative
+ projectile-project-search-path '("~/code/"))
+;; Place your private configuration here

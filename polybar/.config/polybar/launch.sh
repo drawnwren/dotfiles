e945adfeef9424e7f41eaf1fbe9ad53692e919c8
Drawn Wren <drawnwren@protonmail.com>
2022-04-01 17:33:19 -1000

    working on polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,12 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar background &
+polybar --reload background &
 polybar left &
 echo "---" | tee -a /tmp/polybar1.log
-polybar media &
-polybar mediacontrols &
 polybar datetime &
 polybar netstuff &
 polybar volume &
--- profile/.profile
+++ profile/.profile
@@ -1,1 +1,3 @@
 export PATH=$PATH:$HOME/.scripts:$HOME/.cargo/bin:$HOME/.local/bin
+
+export PATH="/home/wing/.local/share/solana/install/active_release/bin:$PATH"
